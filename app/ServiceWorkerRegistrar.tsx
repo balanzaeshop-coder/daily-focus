1c08efd5e959617ad19a6e3f588d4ff38ec22d54
Richard Guth <balanza.eshop@gmail.com>
2026-05-28 21:16:53 +0200

    Fix: unregister old SW before registering new one

--- app/ServiceWorkerRegistrar.tsx
+++ app/ServiceWorkerRegistrar.tsx
@@ -5,6 +5,11 @@ import { useEffect } from 'react'
 export default function ServiceWorkerRegistrar() {
   useEffect(() => {
     if ('serviceWorker' in navigator) {
+      // Unregister old cached service workers first
+      navigator.serviceWorker.getRegistrations().then(regs => {
+        for (const reg of regs) reg.unregister()
+      })
+      // Register fresh
       navigator.serviceWorker.register('/sw.js').catch(() => {})
     }
   }, [])
